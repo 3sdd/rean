b96e67481d8cd58ebafc45e8251a28a8e9a126c4
3sdd <56486103+3sdd@users.noreply.github.com>
2021-02-21 14:49:43 +0900

    index.vueのいらない部分削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,9 @@
 <template>
   <div class="container">
     <div>
-      <h1 style="background-color:red;">{{word}}</h1>
       <nuxt-link to="/create-project" class="block">プロジェクト作成</nuxt-link>
       <nuxt-link to="/open-project" class="block">プロジェクトを開く</nuxt-link>
       <nuxt-link to="/rect-annotator" class="block">debug＿アノテーションページ</nuxt-link>
-      <!-- <Logo /> -->
-      <!-- <h1 class="title">
-        rean
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div> -->
     </div>
   </div>
 </template>
@@ -35,11 +12,6 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  data(){
-    return {
-      word:"hello world!",
-    }
-  }
 })
 </script>
 
@@ -53,33 +25,4 @@ export default Vue.extend({
   text-align: center;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
