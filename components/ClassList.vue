8938b49225eccdda72f7e90caa56103bca9aaef3
3sdd <56486103+3sdd@users.noreply.github.com>
2021-03-25 16:02:58 +0900

    Update ClassList.vue

--- components/ClassList.vue
+++ components/ClassList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h2 class="font-bold text-white mb-3">タグ</h2>
-        <div>
+        <h2 class="font-bold text-black mb-3">タグ</h2>
+        <div v-if="classes.length>=1">
             <div v-for="(className,i) in classes" :key="'class'+i"
                 class="p-1 border border-gray-400 hover:bg-gray-500 hover:text-white"
                 @click="onClick(i)"
